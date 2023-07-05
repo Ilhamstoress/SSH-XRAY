95e6e05ca84138bcdb1427b7614368c6cc7c65b3
rizkyckj <132219364+rizkyckj@users.noreply.github.com>
2023-07-05 20:51:18 +0700

    Update menu-bot.sh

--- data/menu-bot.sh
+++ data/menu-bot.sh
@@ -3,8 +3,8 @@
 #install
 apt update && apt upgrade
 apt install python3 python3-pip git
-git clone https://github.com/Zeadxt/dbasu.git
-unzip dbasu/xolpanel.zip
+git clone https://github.com/JurigVPN/scupdate.git
+unzip scupdate/xolpanel.zip
 pip3 install -r xolpanel/requirements.txt
 pip3 install pillow
 
@@ -42,8 +42,6 @@ END
 
 systemctl start xolpanel 
 systemctl enable xolpanel
-
-sleep 3
 
 clear
 
